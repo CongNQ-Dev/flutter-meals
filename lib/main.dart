6024c8a987f55ae56c91e71d0455effb5910e425
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2024-01-26 16:41:54 +0700

    Adding Cross-Screen Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/data/dummy_data.dart';
+
 import 'package:meals/screens/categories.dart';
-import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,12 +22,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: theme,
-        home: MealsScreen(
-          title: "Some Category",
-          meals: dummyMeals,
-        ) // Todo ...,
+    return MaterialApp(theme: theme, home: CategoriesScreen() // Todo ...,
         );
   }
 }
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+  void _selectedCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: "title", meals: [])));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +26,10 @@ class CategoriesScreen extends StatelessWidget {
             mainAxisSpacing: 20),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () => _selectedCategory(context),
+            )
         ],
       ),
     );
